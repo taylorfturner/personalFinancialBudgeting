0c06c03734b795616e636ecced39c2e67bb7fda4
Taylor Turner <taylorfturner@gmail.com>
2017-12-23 11:21:10 -0500

    update MySQL db create script

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,13 @@
+CREATE TABLE `TRANSACTION` (
+  `transaction_number` int(11) DEFAULT NULL,
+  `timestamp_key` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `TRID` varchar(255) DEFAULT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `debit` varchar(255) DEFAULT NULL,
+  `credit` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `CCNM` (
   `CODE` int(11) DEFAULT NULL,
   `CODEDSCRP` varchar(255) DEFAULT NULL
@@ -14,6 +24,15 @@ CREATE TABLE `BUDGET` (
   `TRID_CODE` int(11) DEFAULT NULL,
   `CREDIT` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+#need to add the create syntax for the func_inc_var_session() function
+
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `r_trans`
+AS SELECT
+   `func_inc_var_session`() AS `ROW_NUM`,if((length(month(`transaction`.`timestamp_key`)) = 1),convert(concat(year(`transaction`.`timestamp_key`),'0',month(`transaction`.`timestamp_key`)) using utf8),convert(concat(year(`transaction`.`timestamp_key`),month(`transaction`.`timestamp_key`)) using utf8)) AS `PERIOD`,sum(1) AS `COUNT`,sum(`transaction`.`debit`) AS `SUM`
+FROM `transaction` group by `PERIOD`;
 
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `r_budget_view`
@@ -67,3 +86,69 @@ AS SELECT
    `r_temp_decay`.`DEBIT` AS `DEBIT`,
    `budget_sum_view`.`INCOME` AS `INCOME`
 FROM (`r_temp_decay` left join `budget_sum_view` on((`r_temp_decay`.`PERIOD` = convert(`budget_sum_view`.`PERIOD` using utf8))));
+
+
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `r_margin_triddecay`
+AS SELECT
+   left(`r_temp_daydecay`.`PERIOD`,4) AS `YEAR`,
+   `r_temp_daydecay`.`PERIOD` AS `PERIOD`,
+   `r_temp_daydecay`.`PERIODKEY` AS `PERIODKEY`,
+   `r_temp_daydecay`.`DAY` AS `DAY`,
+   `r_temp_daydecay`.`TRID` AS `TRID`,
+   `r_temp_daydecay`.`DEBIT` AS `DEBIT`,
+   `budget_sum_tridview`.`INCOME` AS `INCOME`
+FROM (`r_temp_daydecay` left join `budget_sum_tridview` on(((`r_temp_daydecay`.`PERIOD` = convert(`budget_sum_tridview`.`PERIOD` using utf8)) and (`r_temp_daydecay`.`TRID` = `budget_sum_tridview`.`TRID`))));
+
+
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `r_margin_view`
+AS SELECT
+   `func_inc_var_session`() AS `ROW_NUM`,
+   `expense_view`.`PERIOD` AS `PERIOD`,round(`budget_sum_view`.`INCOME`,1) AS `INCOME`,round((`expense_view`.`DEBIT` * -(1)),1) AS `EXPENSES`,round((`budget_sum_view`.`INCOME` + `expense_view`.`DEBIT`),1) AS `MARGIN`,((round((`budget_sum_view`.`INCOME` + `expense_view`.`DEBIT`),1) / round(`budget_sum_view`.`INCOME`,1)) * 100) AS `OP_MARGIN`,
+   `htrid_count`.`HTRID_COUNT` AS `HTRID_COUNT`,
+   `atrid_count`.`ATRID_COUNT` AS `ATRID_COUNT`,
+   `digtrid_count`.`DIGTRID_COUNT` AS `DIGTRID_COUNT`,
+   `ttrid_count`.`TTRID_COUNT` AS `TTRID_COUNT`,
+   `prtrid_count`.`PRTRID_COUNT` AS `PRTRID_COUNT`,
+   `phtrid_count`.`PHTRID_COUNT` AS `PHTRID_COUNT`,
+   `svtrid_count`.`SVTRID_COUNT` AS `SVTRID_COUNT`,
+   `ftrid_count`.`FTRID_COUNT` AS `FTRID_COUNT`
+FROM (((((((((`expense_view` left join `budget_sum_view` on((`expense_view`.`PERIOD` = convert(`budget_sum_view`.`PERIOD` using utf8)))) left join `htrid_count` on((`expense_view`.`PERIOD` = convert(`htrid_count`.`PERIOD` using utf8)))) left join `atrid_count` on((`expense_view`.`PERIOD` = convert(`atrid_count`.`PERIOD` using utf8)))) left join `digtrid_count` on((`expense_view`.`PERIOD` = convert(`digtrid_count`.`PERIOD` using utf8)))) left join `prtrid_count` on((`expense_view`.`PERIOD` = convert(`prtrid_count`.`PERIOD` using utf8)))) left join `phtrid_count` on((`expense_view`.`PERIOD` = convert(`phtrid_count`.`PERIOD` using utf8)))) left join `ftrid_count` on((`expense_view`.`PERIOD` = convert(`ftrid_count`.`PERIOD` using utf8)))) left join `svtrid_count` on((`expense_view`.`PERIOD` = convert(`svtrid_count`.`PERIOD` using utf8)))) left join `ttrid_count` on((`expense_view`.`PERIOD` = convert(`ttrid_count`.`PERIOD` using utf8))));
+
+
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `r_savings`
+AS SELECT
+   if((length(month(`budget`.`DATE_TIME`)) = 1),convert(concat(year(`budget`.`DATE_TIME`),'0',month(`budget`.`DATE_TIME`)) using utf8),convert(concat(year(`budget`.`DATE_TIME`),month(`budget`.`DATE_TIME`)) using utf8)) AS `PERIOD`,
+   `budget`.`TRID_CODE` AS `TRID_CODE`,
+   `budget`.`CREDIT` AS `CREDIT`
+FROM (`budget` left join `transaction` on(((if((length(month(`budget`.`DATE_TIME`)) = 1),convert(concat(year(`budget`.`DATE_TIME`),'0',month(`budget`.`DATE_TIME`)) using utf8),convert(concat(year(`budget`.`DATE_TIME`),month(`budget`.`DATE_TIME`)) using utf8)) = if((length(month(`transaction`.`timestamp_key`)) = 1),convert(concat(year(`transaction`.`timestamp_key`),'0',month(`transaction`.`timestamp_key`)) using utf8),convert(concat(year(`transaction`.`timestamp_key`),month(`transaction`.`timestamp_key`)) using utf8))) and (`budget`.`TRID_CODE` = `transaction`.`transaction_number`)))) where (`budget`.`TRID_CODE` = '801');
+
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `r_summary_view` AS (select `budget_view`.`PERIOD` AS `PERIOD`,`budget_view`.`TRID` AS `TRID`,`budget_view`.`TRID_CODE` AS `TRID_CODE`,(case when isnull(`transaction_view`.`DEBIT`) then 0.000 else `transaction_view`.`DEBIT` end) AS `DEBIT`,`budget_view`.`CREDIT` AS `CREDIT`,round(((((case when isnull(`transaction_view`.`DEBIT`) then 0.0000 else `transaction_view`.`DEBIT` end) * -(1)) - `budget_view`.`CREDIT`) * -(1)),2) AS `VAR`,if((((((case when isnull(`transaction_view`.`DEBIT`) then 0.000 else `transaction_view`.`DEBIT` end) * -(1)) - `budget_view`.`CREDIT`) * -(1)) < -(0)),1,0) AS `VAR_FLG` from (`budget_view` left join `transaction_view` on(((convert(`budget_view`.`PERIOD` using utf8) = `transaction_view`.`PERIOD`) and (`budget_view`.`TRID` = `transaction_view`.`TRID`) and (`budget_view`.`TRID_CODE` = `transaction_view`.`TRID_CODE`)))) order by `transaction_view`.`PERIOD` desc);
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `r_temp_daydecay`
+AS SELECT
+   if((length(month(`transaction`.`timestamp_key`)) = 1),convert(concat(year(`transaction`.`timestamp_key`),'0',month(`transaction`.`timestamp_key`)) using utf8),convert(concat(year(`transaction`.`timestamp_key`),month(`transaction`.`timestamp_key`)) using utf8)) AS `PERIOD`,concat(if((length(month(`transaction`.`timestamp_key`)) = 1),convert(concat(year(`transaction`.`timestamp_key`),'0',month(`transaction`.`timestamp_key`)) using utf8),convert(concat(year(`transaction`.`timestamp_key`),month(`transaction`.`timestamp_key`)) using utf8)),dayofmonth(`transaction`.`timestamp_key`)) AS `PERIODKEY`,dayofmonth(`transaction`.`timestamp_key`) AS `DAY`,
+   `transaction`.`TRID` AS `TRID`,(sum(`transaction`.`debit`) * -(1)) AS `DEBIT`
+FROM `transaction` where ((`transaction`.`debit` * -(1)) > 0) group by `PERIOD`,`PERIODKEY`,`DAY`,`transaction`.`TRID`;
+
+
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `r_temp_decay`
+AS SELECT
+   if((length(month(`transaction`.`timestamp_key`)) = 1),convert(concat(year(`transaction`.`timestamp_key`),'0',month(`transaction`.`timestamp_key`)) using utf8),convert(concat(year(`transaction`.`timestamp_key`),month(`transaction`.`timestamp_key`)) using utf8)) AS `PERIOD`,concat(if((length(month(`transaction`.`timestamp_key`)) = 1),convert(concat(year(`transaction`.`timestamp_key`),'0',month(`transaction`.`timestamp_key`)) using utf8),convert(concat(year(`transaction`.`timestamp_key`),month(`transaction`.`timestamp_key`)) using utf8)),dayofmonth(`transaction`.`timestamp_key`)) AS `PERIODKEY`,dayofmonth(`transaction`.`timestamp_key`) AS `DAY`,(sum(`transaction`.`debit`) * -(1)) AS `DEBIT`
+FROM `transaction` where ((`transaction`.`debit` * -(1)) > 0) group by `PERIOD`,`PERIODKEY`,`DAY`;
+
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `r_transactions`
+AS SELECT
+   `transaction`.`transaction_number` AS `transaction_number`,
+   `transaction`.`timestamp_key` AS `timestamp_key`,
+   `transaction`.`description` AS `description`,
+   `transaction`.`TRID` AS `TRID`,
+   `transaction`.`memo` AS `memo`,
+   `transaction`.`debit` AS `debit`,
+   `transaction`.`credit` AS `credit`
+FROM `transaction`;
